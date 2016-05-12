8982f85ddc1ad2a6ead3d7068a3e8960e1918016
Marcin Ignac <marcin.ignac@gmail.com>
2016-05-12 12:36:12 +0100

    Fixed margin calculation

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-
 function grid(w, h, nw, nh, margin){
-    var max =  nw * nh;
-    var cw = Math.floor(w / nw);
-    var ch = Math.floor(h / nh);
+    var cw = Math.floor((w - margin - margin * nw) / nw);
+    var ch = Math.floor((h - margin - margin * nh) / nh);
     var cells = [];
     for(var y = 0; y < nh; ++y){
         for(var x = 0; x < nw; ++x){
             cells.push([
-                x * cw + margin,
-                y * ch + margin,
-                cw - 2 * margin,
-                ch - 2 * margin
+                x * cw + (x + 1) * margin,
+                y * ch + (y + 1) * margin,
+                cw,
+                ch
             ]);
-            if(cells.length >= max){
-                return cells;
-            }
         }
     }
+    return cells;
 }
+
+module.exports = grid;
