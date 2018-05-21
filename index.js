db88a19d11afc9ef0c48617b335415510c6a9969
Marcin Ignac <marcin.ignac@gmail.com>
2018-05-21 08:55:16 +0100

    Change formatting to standard

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function grid(w, h, nw, nh, margin){
-    var cw = Math.floor((w - margin - margin * nw) / nw);
-    var ch = Math.floor((h - margin - margin * nh) / nh);
-    var cells = [];
-    for(var y = 0; y < nh; ++y){
-        for(var x = 0; x < nw; ++x){
-            cells.push([
-                x * cw + (x + 1) * margin,
-                y * ch + (y + 1) * margin,
-                cw,
-                ch
-            ]);
-        }
+function grid (w, h, nw, nh, margin) {
+  var cw = Math.floor((w - margin - margin * nw) / nw)
+  var ch = Math.floor((h - margin - margin * nh) / nh)
+  var cells = []
+  for (var y = 0; y < nh; ++y) {
+    for (var x = 0; x < nw; ++x) {
+      cells.push([
+        x * cw + (x + 1) * margin,
+        y * ch + (y + 1) * margin,
+        cw,
+        ch
+      ])
     }
-    return cells;
+  }
+  return cells
 }
 
-module.exports = grid;
+module.exports = grid
